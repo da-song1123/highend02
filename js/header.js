427e5dbc65436f4a65a4bb11a04575b571c30d5a
da-song1123 <dsong1123@gmail.com>
2023-04-18 09:55:02 +0900

    페이지 생성

--- js/header.js
+++ js/header.js
@@ -1,3 +1,19 @@
 function home(e){
     window.location.href='#'
 }
+
+function about(){
+    window.location.href='../about.html'
+}
+
+function facilities(){
+    window.location.href='../facilities.html'
+}
+
+function membership(){
+    window.location.href='../membership.html'
+}
+
+function community(){
+    window.location.href='../community.html'
+}
